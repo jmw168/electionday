78430962490cc705fbbe754d9ba8b1ef4a7dbdd8
jan <jan-matthis.waack@theo.physik.uni-giessen.de>
2021-01-10 23:00:19 +0100

    Update GOP algorithmus und kleine bugfixes

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # electionday
+
 Eine Sammlung von Python-Skripten für die Politik-Simulation Election-Day (election-day.de)
 
 Hier können die Programme Online ausgeführt werden:
 [![Run on Repl.it](https://repl.it/badge/github/jmw168/electionday)](https://repl.it/@jmw168/electionday)
 
-Um die Anzahl an Delegierten zu berechnen, die jeder Staat zum Wahlparteitag der Republikaner entsendet, geben Sie `python skripte/GOP_delegierte.py` ein
+Mit dem button `Run` wird die aktuelle Version im main-Branch auf repl.it übertragen.
+
+Um die Anzahl an Delegierten zu berechnen, die jeder Staat zum Wahlparteitag der Republikaner entsendet, geben
+Sie `python Skripte/GOP_delegierte.py` ein
---
+++ Skripte/GOP_delegierte.py
@@ -0,0 +1,33 @@
+import numpy as np
+import yaml
+
+
+def lese_yaml(dateiname):
+    with open('Daten/' + dateiname + '.yaml') as datei:
+        daten = yaml.safe_load(datei)
+    return daten
+
+
+def main():
+    allgemein = lese_yaml('Allgemein')
+    abgeordnete = lese_yaml('Abgeordnete')
+    wahlergebnisse = lese_yaml('Wahlergebnisse')
+    senat = lese_yaml('Senat')
+    wahlergebnis = wahlergebnisse[-1]
+    delegierte = {}
+    for staat in allgemein['Staaten'].keys():
+        anzahl_abgeordnete = len([abgeordneter for abgeordneter in abgeordnete if abgeordneter['Staat'] == staat])
+        bonus = int(np.ceil(0.06 * wahlergebnis[staat]['WM'])) if wahlergebnis[staat]['R'] > wahlergebnis[staat][
+            'D'] else 0
+        republikanische_abgeordnete = len([abgeordneter for abgeordneter in abgeordnete if
+                                           abgeordneter['Staat'] == staat and abgeordneter['Partei'] == 'R'])
+        republikanische_senatoren = len(
+            [senator for senator in senat if senator['Staat'] == staat and senator['Partei'] == 'R'])
+        anzahl = 10 + 3 * anzahl_abgeordnete + bonus + (
+                    republikanische_abgeordnete > 0.5 * anzahl_abgeordnete) + republikanische_senatoren
+        delegierte.update({staat: anzahl})
+    print(delegierte)
+
+
+if __name__ == '__main__':
+    main()
